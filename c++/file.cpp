83155e6339635af56c582b63f39b6310838645ec
Cristian Eulises Sanchez Ramirez <scristianeulises@gmail.com>
2024-09-29 20:10:49 -0400

    fix formatting

--- c++/file.cpp
+++ c++/file.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 #include <string>
 
-std::string mergeAlternately(const std::string &word1, const std::string word2)
-{
+std::string mergeAlternately(const std::string &word1, const std::string word2) {
+
     std::string merged;
     size_t length = std::max(word1.length(), word2.length());
 
-    for(size_t i = 0; i < length; i ++)
-    {
-        if(word1.length() > i) { merged += word1[i]; }
-        if(word2.length() > i) { merged += word2[i]; }
+    for (size_t i = 0; i < length; i++) {
+        if (word1.length() > i) {
+            merged += word1[i];
+        }
+        if (word2.length() > i) {
+            merged += word2[i];
+        }
     }
-    
+
     return merged;
 }
 
-int main()
-{
+int main() {
     std::cout << mergeAlternately("abc", "ZP") << std::endl;
     std::cout << mergeAlternately("world", "HELL") << std::endl;
     std::cout << mergeAlternately("number", "CORRIEND") << std::endl;
