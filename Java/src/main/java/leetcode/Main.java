86b5d1ced0f2182a085fa7b5017f64ab7732ca2f
Cristian Eulises Sanchez Ramirez <scristianeulises@gmail.com>
2024-10-22 12:03:26 -0400

    java recorded

--- Java/src/main/java/leetcode/Main.java
+++ Java/src/main/java/leetcode/Main.java
@@ -2,14 +2,13 @@ package leetcode;
 
 import leetcode.exercises.GreatestCommonDivisorOfStrings;
 // import leetcode.exercises.MergeAlternately;
+import leetcode.exercises.KidsWithTheGreatestNumberOfCandies;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1. " + GreatestCommonDivisorOfStrings.exec("ABCABC", "ABC"));
-        System.out.println("2. " + GreatestCommonDivisorOfStrings.exec("ABABAB", "ABAB"));
-        System.out.println("3. " + GreatestCommonDivisorOfStrings.exec("XYZXYZXYZ", "XYZ"));
-        System.out.println("4. " + GreatestCommonDivisorOfStrings.exec("LEET", "CODE"));
-        System.out.println("5. " + GreatestCommonDivisorOfStrings.exec("ABCABCABC", "ABCAAA"));
+        System.out.println("1. " + KidsWithTheGreatestNumberOfCandies.exec(new int[] { 2, 3, 5, 1, 3 }, 3));
+        System.out.println("2. " + KidsWithTheGreatestNumberOfCandies.exec(new int[] { 4, 2, 1, 1, 2 }, 1));
+        System.out.println("3. " + KidsWithTheGreatestNumberOfCandies.exec(new int[] { 12, 1, 12 }, 10));
 
     }
 }
--- Java/src/main/java/leetcode/exercises/KidsWithTheGreatestNumberOfCandies.java
+++ Java/src/main/java/leetcode/exercises/KidsWithTheGreatestNumberOfCandies.java
@@ -9,9 +9,9 @@ public class KidsWithTheGreatestNumberOfCandies {
     public static List<Boolean> exec(int[] candies, int extraCandies) {
         List<Boolean> kidsWithExtraCandies = new ArrayList<>();
         int maxValue = Arrays.stream(candies).max().getAsInt();
-        for (int candy : candies) {
+        for(int candy : candies){
             kidsWithExtraCandies.add(candy + extraCandies >= maxValue);
-        }
+        }        
         return kidsWithExtraCandies;
     }
 
