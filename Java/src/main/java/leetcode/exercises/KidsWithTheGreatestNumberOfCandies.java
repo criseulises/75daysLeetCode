52b640c024f37ba2b77bd23047ce9361edd01cd4
Cristian Eulises Sanchez Ramirez <scristianeulises@gmail.com>
2024-10-22 10:01:10 -0400

    1431. Kids With the Greatest Number of Candies resolved in java

---
+++ Java/src/main/java/leetcode/exercises/KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,18 @@
+package leetcode.exercises;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class KidsWithTheGreatestNumberOfCandies {
+
+    public static List<Boolean> exec(int[] candies, int extraCandies) {
+        List<Boolean> kidsWithExtraCandies = new ArrayList<>();
+        int maxValue = Arrays.stream(candies).max().getAsInt();
+        for (int candy : candies) {
+            kidsWithExtraCandies.add(candy + extraCandies >= maxValue);
+        }
+        return kidsWithExtraCandies;
+    }
+
+}
