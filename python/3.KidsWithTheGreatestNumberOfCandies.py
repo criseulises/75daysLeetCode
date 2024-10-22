61581cf8d27c4e32c2c3e3518471a5345957f36e
Cristian Eulises Sanchez Ramirez <scristianeulises@gmail.com>
2024-10-22 11:42:53 -0400

    python recorded

--- python/3.KidsWithTheGreatestNumberOfCandies.py
+++ python/3.KidsWithTheGreatestNumberOfCandies.py
@@ -1,9 +1,11 @@
 def kidsWithCandies(candies, extraCandies):
     kidsWithExtraCandies = []
     maxValue = max(candies)
-    for kid in candies:
-        kidsWithExtraCandies.append(kid + extraCandies >= maxValue)
+    for candy in candies:
+        kidsWithExtraCandies.append(candy + extraCandies >= maxValue)
     return kidsWithExtraCandies
 
 
 print(kidsWithCandies([2, 3, 5, 1, 3], 3))
+print(kidsWithCandies([4, 2, 1, 1, 2], 1))
+print(kidsWithCandies([12, 1, 12], 1))
