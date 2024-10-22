06056778df8af74e236f189e33cddfbb026c255c
Cristian Eulises Sanchez Ramirez <scristianeulises@gmail.com>
2024-10-22 11:54:02 -0400

    javascript recorded

--- javascript/3.KidsWithTheGreatestNumberOfCandies.js
+++ javascript/3.KidsWithTheGreatestNumberOfCandies.js
@@ -1,8 +1,10 @@
 function kidsWithCandies(candies, extraCandies) {
     let kidsWithExtraCandies = []
-    const max = Math.max(...candies)
-    candies.forEach((kid) => kidsWithExtraCandies.push(kid + extraCandies >= max))
+    const maxValue = Math.max(...candies)
+    candies.forEach((candy) => kidsWithExtraCandies.push(candy + extraCandies >= maxValue))
     return kidsWithExtraCandies
 }
 
 console.log(kidsWithCandies([2, 3, 5, 1, 3], 3))
+console.log(kidsWithCandies([4, 2, 1, 1, 2], 1))
+console.log(kidsWithCandies([12, 1, 12], 10))
