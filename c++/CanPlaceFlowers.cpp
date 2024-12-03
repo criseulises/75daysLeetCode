66ea9594f4c0034ed83e8f8fde4eef401c283d5d
Cristian Eulises Sanchez Ramirez <scristianeulises@gmail.com>
2024-12-03 16:00:06 -0400

    can place flowers c++ - done

---
+++ c++/CanPlaceFlowers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+bool canPlaceFlowers(std::vector<int> &flowerbed, int n)
+{
+    for (size_t i = 0; i < flowerbed.size(); i++)
+    {
+        if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0))
+        {
+            flowerbed[i] = 1;
+            n--;
+            if (n <= 0) return true;
+        }
+    }
+
+    return n <= 0;
+}
+
+int main()
+{
+    std::vector<int> flowerbed = {1, 0, 0, 0, 1};
+    std::cout << canPlaceFlowers(flowerbed, 2) << std::endl;
+    return 0;
+}
